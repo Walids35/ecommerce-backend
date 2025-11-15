62cba716a3a7933a023ff96cec6448e940b31c8b
Walid Siala <wsiala4@gmail.com>
2025-11-15 14:56:20 +0100

    user schema definition

---
+++ src/db/schema/users.ts
@@ -0,0 +1,15 @@
+import { pgEnum, pgTable, timestamp, uuid, varchar } from "drizzle-orm/pg-core";
+
+export const userRoleEnum = pgEnum("user_role", ["admin", "superadmin"]);
+
+export const user = pgTable("user", {
+  id: uuid("id").primaryKey().defaultRandom(),
+
+  email: varchar("email", { length: 255 }).notNull().unique(),
+  password: varchar("password", { length: 255 }).notNull(),
+
+  name: varchar("name", { length: 255 }).notNull(),
+  role: userRoleEnum("role").default("admin").notNull(),
+
+  createdAt: timestamp("createdAt").defaultNow().notNull(),
+});
