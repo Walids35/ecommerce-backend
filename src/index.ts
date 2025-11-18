5ea7ec896b391d3f09b41523632c44c8b79ae54c
Walid Siala <wsiala4@gmail.com>
2025-11-18 11:56:06 +0100

    Added cookie parser

--- src/index.ts
+++ src/index.ts
@@ -5,6 +5,7 @@ import { errorHandler } from './middlewares/errorHandler';
 import { notFound } from './middlewares/notFound';
 import authRoutes from './modules/auth/auth.routes';
 import { seedDatabase } from './db/database-seeding';
+import cookieParser from 'cookie-parser';
 
 dotenv.config();
 
@@ -13,17 +14,18 @@ const PORT = process.env.PORT || 3000;
 
 // Middlewares
 app.use(cors({
-  origin: process.env.FRONTEND_URL || 'http://localhost:3000',
+  origin: process.env.FRONTEND_URL || 'http://localhost:5173',
   credentials: true
 }));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
 
 // Routes
 app.use('/api/auth', authRoutes);
 
 app.get('/health', (req, res) => {
-  res.json({ status: 'OK', message: 'Server is running' });
+  res.json({ status: 'OK', message: 'Server is running' })
 });
 
 app.use(notFound);
